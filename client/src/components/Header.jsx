894b864306791812a5921c213b14737186cb9028
kapillohanii <kapillohani023@gmail.com>
2023-08-18 16:58:22 +0530

    responsive changes

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,17 +1,29 @@
-import React from "react";
+import React, { useState } from "react";
 import Search from './Search';
 import '../App.css'
 
 function Header() {
+    const [showNav, setShowNav] = useState(false);
+
+    const toggleNav = () => {
+        setShowNav(!showNav);
+    };
+
     return ( 
         <div className="sticky-top bg-white p-3">
             <div className="container d-flex flex-column flex-md-row align-items-center">
                 <div><a href='/' style={{color:'black'}}><h1 className="H1">Library Manager</h1></a></div>
                 <div><Search /></div>
-                <div>
+                <div className={`nav-links ${showNav ? 'show' : ''}`}>
+                    <button className="bton red" onClick={toggleNav}>x</button>
                     <a href="/issue" className="Nav">Issue Books</a>
                     <a href="/return" className="Nav">Return Books</a>
                     <a href="/pending" className="Nav">Pending Transactions</a>
+                </div>
+                <div className="burger-icon" onClick={toggleNav}>
+                    <div className={`bar${showNav ? ' active' : ''}`}></div>
+                    <div className={`bar${showNav ? ' active' : ''}`}></div>
+                    <div className={`bar${showNav ? ' active' : ''}`}></div>
                 </div>
             </div>
         </div> 
